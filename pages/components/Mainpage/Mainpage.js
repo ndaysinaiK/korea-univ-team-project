5be7b36e343e3730654eede5108882dc39fcdb40
ndaysinaiK <kabulo_nday@kookmin.ac.kr>
2022-05-29 03:07:59 +0900

    29/05/2022 03:07:59 snk

--- pages/components/Mainpage/Mainpage.js
+++ pages/components/Mainpage/Mainpage.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import {BsCart4} from 'react-icons/bs'
+import {ImFileText2} from 'react-icons/im'
 
 function Mainpage() {
   return (
@@ -33,24 +35,24 @@ function Mainpage() {
             </div>
 
             <div className="flex flex-col items-center justify-center">
-                <div className="bg-white shadow-md rounded-md w-5/6 z-10 -mt-10 h-40 ">
+                <div className="bg-white shadow-md rounded-md w-5/6 z-10 -mt-10 h-50 ">
                     <div>
                         <div className="flex justify-between p-3 mt-2">
-                            <p className="border flex flex-col items-center justify-center p-1 text-xs border-black ml-3">
-                                <a>XXXX님의 Ment 보유량</a>
-                                <a>123 Mental Coins</a>
+                            <p className="border flex flex-col items-center justify-center w-2/4 p-1 text-xs border-black ml-3 mr-2">
+                                <a style={{fontSize:'9px'}}>XXXX님의 Mint 보유량</a>
+                                <a style={{fontSize:'10px'}} className="font-bold">123 Mental Coins</a>
                             </p>
-                            <p className=" rounded-full bg-green-500 border flex items-center justify-center text-white p-3 text-sm border-black mr-3">충전하기</p>
+                            <p className=" w-2/4 rounded-full bg-green-500 border flex items-center justify-center text-white p-3 text-sm border-black mr-3">충전하기</p>
                         </div>
 
                         <div className="flex justify-between p-3">
-                            <p className="border flex flex-col items-center justify-center p-1 text-xs border-black ml-3">
-                                <a>XXXX님의 Ment 보유량</a>
-                                <a>설문하기</a>
+                            <p className="border flex flex-col w-2/4 items-center justify-center p-1 text-xs border-black ml-3">
+                                <a className="text-3xl"><ImFileText2/></a>
+                                <a className="animate-bounce mt-3">설문하기</a>
                             </p>
-                            <p className="border p-1 text-xs border-black ml-3">
-                                <a>XXXX님의 Ment 보유량</a> < br />
-                                <a>데이터 구매</a>
+                            <p className="border p-1 text-xs flex flex-col items-center justify-center w-2/4 border-black ml-3">
+                                <a className="text-3xl"><BsCart4/></a>
+                                <a className="mt-3">데이터 구매</a>
                             </p>
                         </div>
                         
@@ -60,6 +62,11 @@ function Mainpage() {
                 <div className="bg-blue-50 w-5/6 rounded-lg mt-6 text-xs flex items-center justify-around p-2">
                     <p>매일매일 카드 조회하고 랜덤 포인트 쫍으세요</p>
                     <p className="text-green-500 font-bold"> {'>'} </p>
+                </div>
+
+                <div className="border border-black w-5/6 p-2 mt-3">
+                    Survey
+
                 </div>
 
             </div>
